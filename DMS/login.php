40533caccd6be5269f8652ada5410d48b67d92f6
Md. Shakil Hossain <shakiljusc15@gmail.com>
2023-04-03 05:13:14 +0600

    Upload

---
+++ DMS/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login page</title>
+    <style>
+        body{
+            background: lightblue;
+           margin-left: 30px;
+        }
+      
+        .loginbtn{
+            width: 50%;
+            height: 300px;
+            background-image: url(images/table1.jpg);
+           
+        }
+        .div2{
+            margin-top: 100px;
+
+        }
+        button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  border-radius: 5px 5px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+       
+  
+    </style>
+</head>
+<body>
+
+    <center>
+    <div class="loginbtn">
+            <!-- <p>Login</p> -->
+            <div class="div2">
+            <h1 style="color: white; ">ADMIN LOG IN</h1>
+         <form action="login process.php" method="POST">
+    
+                   <input type="text" style="width: 50%; height:30px" id="user" name="username" placeholder="username"/><br><br>
+                   <input type="Password" style="width: 50%; height:30px" id="pass" name="password" placeholder="password"/><br><br>
+                   <button type="submit" style="width: 15%; height:30px" id="btn" name="login" default>login</button>
+                   </div>
+                   
+         </form>
+         </div>
+
+    </center>
+
+</body>
+</html>
---
+++ DMS/login_db.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 28, 2022 at 12:29 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

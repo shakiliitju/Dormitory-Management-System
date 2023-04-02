dee356bf23e1cb606153d8bade890f071960043e
Md. Shakil Hossain <shakiljusc15@gmail.com>
2023-04-03 05:29:00 +0600

    database update

--- DMS/dashboard/connection.php
+++ DMS/dashboard/connection.php
@@ -3,7 +3,7 @@ $sname = "localhost";
 $uname = "root";
 $password = "";
 mysqli_connect('localhost','root',''); 
-$conn =  mysqli_connect($sname, $uname, $password, 'final_dbms'); 
+$conn =  mysqli_connect($sname, $uname, $password, 'DMS'); 
 ?>
 <!-- if(!$conn)
 {
--- DMS/database connection.php
+++ DMS/database connection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","login_db");
+$con = mysqli_connect("localhost","root","","login");
 
 // Check connection
 if (mysqli_connect_errno())
